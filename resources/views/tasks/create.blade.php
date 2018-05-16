878390e92d8669038d3f567204ed6bc5216f570c
EC2 Default User <ec2-user@ip-172-31-44-155.us-east-2.compute.internal>
2018-05-16 13:10:00 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,6 +7,9 @@
     
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}    
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -8,6 +8,9 @@
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
+
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
 
